8ade1ad30edd275963f085e583653b0b770bdeb7
Garrett Cintron <grrttcintron@gmail.com>
2020-12-23 14:29:16 -0800

    lab 11 functionality

--- events.js
+++ events.js
@@ -5,10 +5,6 @@
 
 // const events = new Events();
 const io = require('socket.io-client');
-const host = 'http://localhost:3000';
 
-const CAPSConnection = io.connect(host);
 
 //CAPSConnection.emit('hi', {payload: 'payload'});
-
-module.exports = CAPSConnection;
