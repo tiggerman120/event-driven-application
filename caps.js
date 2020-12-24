37067b48b48a58c5f27b1a1d0245b232f2ffb38a
Garrett Cintron <grrttcintron@gmail.com>
2020-12-23 22:31:25 -0800

    lab 13 stuff

--- caps.js
+++ caps.js
@@ -1,30 +1,34 @@
 'use strict';
-const uuid = require('uuid').v4;
+
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 const io = require('socket.io')(port);
 const caps = io.of('/caps-namespace');
+
 console.log(port);
 io.on('connection', (socket) => {
   console.log('you are connected to the server', socket.id);
 });
 
-
+caps.on('connection', (socket) => {
+  console.log('you are connected to caps-namespace', socket.id);
+  socket.on('join', room => {
+    console.log(`${socket.id} joining ${room}`);
+    socket.join(room);
+  });
+});
 
 caps.on('connection', (socket) => {
   function logger(payload) {
     let timestamp = new Date().toString();
     console.log('EVENT', { timestamp, payload });
   }
-  console.log('you are connected to caps-namespace', socket.id);
-  socket.on('join', room => {
-    console.log(`${socket.id} joining ${room}`);
-    socket.join(room);
-  });
+
 
   socket.on('pickup', (payload) => {
     console.log('line 25');
     logger(payload);
+    //let queue.chores[id] = payload
     caps.emit('pickup', payload);
   });
 
@@ -39,19 +43,11 @@ caps.on('connection', (socket) => {
     logger(payload);
     caps.to(payload.store).emit('delivered', payload);
   });
+
+  socket.on('received', (payload) => {
+    console.log(payload, 'received route');
+
+  });
 });
 
-const queue = {
-  chores: {},
-};
 
-// family.on('connection', socket => {
-//   socket.on('new chore', payload => {
-//     console.log('in the HUB - heard a new core', payload);
-//     const id = uuid();
-//     queue.chores[id] = payload;
-
-//     socket.emit('added');
-//     family.emit('chore', {id, payload});
-//   });
-//});
